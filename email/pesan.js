1b1e624b6b5d0b858570ba17b7a680efa971542a
watama15 <109936711+watama15@users.noreply.github.com>
2022-10-03 20:13:42 +0700

    Update pesan.js

--- email/pesan.js
+++ email/pesan.js
@@ -1,6 +1,6 @@
 // virtex lu
 const gmail1 = `
-Hay Fitur eror yang anda gunakan sudah di Fix oleh moderator Fangz BOT
+Hay Fitur eror yang anda gunakan sudah di Fix oleh moderator Anya BOT
 Jika ada fitur eror lainya ketik .report ya
 Agar kami tangani secepatnya 
 
